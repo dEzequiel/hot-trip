667755ac5a5124ec3502292705496bbbd1482de2
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-04-16 17:36:22 +0200

    feat: add main and tests

--- hot-trip/src/main/java/edu/poniperro/App.java
+++ hot-trip/src/main/java/edu/poniperro/App.java
@@ -4,10 +4,37 @@ package edu.poniperro;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        // Oficina oficina = new Oficina();
+
+        // Pedido pedido = new PedidoInternacional("Comarca", 10);
+        // TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
+        //                                             (PedidoInternacional) pedido);
+
+        // System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+
+        // pedido = new PedidoInternacional("Mordor", 10);
+        // tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
+
+        // System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+
+        // pedido = new PedidoPeligrosoOrden("Cima de los vientos",
+        //                                   "No urgarse en las uñas con este puñal");
+        // TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+
+        // System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+
+        // pedido = new PedidoPeligrosoOrden("Monte del destino",
+        //                                   "No ponerselo en el dedo");
+        // peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+
+        // System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+
+        /**
+         * Los pedidos multiples se completan en el ultimo de los casos test
+         */
     }
 }
