d95a21c53f98c1054c48a1303fbf5e12d7461e4b
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-18 21:28:26 +0200

    feat: finish main app

--- hot-trip/src/main/java/edu/poniperro/App.java
+++ hot-trip/src/main/java/edu/poniperro/App.java
@@ -1,5 +1,14 @@
 package edu.poniperro;
 
+import edu.poniperro.interfaces.Pedido;
+import edu.poniperro.interfaces.PedidoPeligroso;
+import edu.poniperro.interfaces.TratamientoPedido;
+import edu.poniperro.pedidos.PedidoInternacional;
+import edu.poniperro.pedidos.PedidoPeligrosoOrden;
+import edu.poniperro.procesadores.Oficina;
+import edu.poniperro.tratamientos.TratamientoPedidoInternacional;
+import edu.poniperro.tratamientos.TratamientoPedidoPeligroso;
+
 /**
  * Hello world!
  *
@@ -8,30 +17,30 @@ public class App
 {
     public static void main(String[] args) {
 
-        // Oficina oficina = new Oficina();
+        Oficina oficina = new Oficina();
 
-        // Pedido pedido = new PedidoInternacional("Comarca", 10);
-        // TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
-        //                                             (PedidoInternacional) pedido);
+        Pedido pedido = new PedidoInternacional("Comarca", 10);
+        TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
+                                                    (PedidoInternacional) pedido);
 
-        // System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
-        // pedido = new PedidoInternacional("Mordor", 10);
-        // tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
+        pedido = new PedidoInternacional("Mordor", 10);
+        tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
 
-        // System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
-        // pedido = new PedidoPeligrosoOrden("Cima de los vientos",
-        //                                   "No urgarse en las uñas con este puñal");
-        // TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+        pedido = new PedidoPeligrosoOrden("Cima de los vientos",
+                                          "No urgarse en las uñas con este puñal");
+        TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
 
-        // System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
 
-        // pedido = new PedidoPeligrosoOrden("Monte del destino",
-        //                                   "No ponerselo en el dedo");
-        // peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+        pedido = new PedidoPeligrosoOrden("Monte del destino",
+                                          "No ponerselo en el dedo");
+        peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
 
-        // System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
 
         /**
          * Los pedidos multiples se completan en el ultimo de los casos test
