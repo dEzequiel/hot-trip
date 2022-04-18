a3398931f95dc5e9e1a61faf236a8c97b10bc1c9
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-18 20:51:33 +0200

    feat: implements Oficina class method to print treatment status

--- hot-trip/src/main/java/edu/poniperro/procesadores/Oficina.java
+++ hot-trip/src/main/java/edu/poniperro/procesadores/Oficina.java
@@ -1,6 +1,8 @@
 package edu.poniperro.procesadores;
 
+import edu.poniperro.interfaces.Pedido;
 import edu.poniperro.interfaces.Procesador;
+import edu.poniperro.interfaces.Status;
 import edu.poniperro.interfaces.TratamientoPedido;
 
 public class Oficina implements Procesador {
@@ -12,9 +14,15 @@ public class Oficina implements Procesador {
         if (!tratamiento.tratar()) {
             return false;
         }
-
         return true;
 
-    }
+    };
 
+    public String printarStatus(boolean estado, Pedido pedido) {
+        if (estado) {
+            return pedido.destino() + " " + Status.ACEPTADO.name();
+        } else {
+            return pedido.destino() + " " +Status.RECHAZADO.name();
+        }
+    };
 }
